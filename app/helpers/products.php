5ec53189ad23dbdc2318a3298d61669e3a6636d0
joicepassos72 <joicepassos72@gmail.com>
2023-03-14 23:55:27 -0300

    - main

---
+++ app/helpers/products.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    1 => [
+        'name' => 'Product 1',
+        'price' => 100,
+    ],
+    2 => [
+        'name' => 'Product 2',
+        'price' => 200,
+    ],
+    3 => [
+        'name' => 'Product 3',
+        'price' => 300,
+    ],
+    4 => [
+        'name' => 'Product 4',
+        'price' => 400,
+    ],
+    5 => [
+        'name' => 'Product 5',
+        'price' => 500,
+    ],
+];
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$products = require __DIR__ . '/../app/helpers/products.php';
+
+var_dump($products);
